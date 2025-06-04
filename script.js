9f760b26e66ecef8e93ac255e9cb7b377b484454
Sreecharan492 <sreecharan492@gmail.com>
2025-06-04 17:40:46 +0530

    Updated

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function greetUser() {
   if (name.trim() === "") {
     alert("Hey! Type something before clicking 😅");
   } else {
-    alert(`Hello ${name}! You're officially a JavaScript wizard now 🧙‍♂️⚡`);
+    alert(`Hello ${name}! How's your day 🧙‍♂️⚡`);
   }
 }
